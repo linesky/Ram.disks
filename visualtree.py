e7670bf9ba3fbf2a578809ea74917be5992e2067
sky line <32512284+linesky@users.noreply.github.com>
2024-10-30 14:50:25 +0000

    Add files via upload

--- visualtree.py
+++ visualtree.py
@@ -1,8 +1,15 @@
 code=""
 last=0
 ttrue=True
+files=input("file data name? ")
+try:
+    f1=open(files,"r")
+    code=f1.read()
+    f1.close()
+except:
+    aa=0
 while ttrue:
-    lines=input("get text?")
+    lines=input("out? ")
     lines=lines.strip()
     if lines=="":
         ttrue=False
@@ -13,12 +20,13 @@ while ttrue:
         for c in range(len(sp)):
             sp1=[]
             n1=c
-            sp1=sp[:n1]
+            sp1=sp[:n1+1]
             s1=",".join(sp1)
             
             s2="    "*c
+            ss2="    "*(c+1)
             s1=s2+"#"+s1+"\n"
-            s4=s1+s2+"if value=="+sp[c]+":\n"+s2+"values="+sp[c]+"\n"
+            s4=s1+s2+"if value=="+sp[c]+":\n"+ss2+"values="+sp[c]+"\n"
             n4=len(s4)
             nn=code.find(s1)
             if nn<0:
@@ -34,6 +42,12 @@ while ttrue:
                 m=nn+n4+1
 
         print(code)
+try:
+    f1=open(files,"w")
+    f1.write(code)
+    f1.close()
+except:
+    print("error on write file")
             
             
         
